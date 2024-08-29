bd25c148a7a3560f0071cf96a0b09adf3ad369ef
Phan Hồng Sơn <enochphann@gmail.com>
2024-08-29 14:37:48 +0700

    Update source code for Lesson 13

---
+++ Javascript Course/javascript-amazon-project/scripts/amazon.js
@@ -0,0 +1,112 @@
+let productsHTML = '';
+
+products.forEach(product => {
+  productsHTML += `
+    <div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
+      </div>
+
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
+
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
+
+      <div class="product-price">
+        $${(product.priceCents / 100).toFixed(2)}
+      </div>
+
+      <div class="product-quantity-container">
+        <select class="js-quantity-selector-${product.id}">
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart js-added-to-cart-${product.id}">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+        Add to Cart
+      </button>
+    </div>
+  `;
+});
+
+document.querySelector('.js-products-grid')
+  .innerHTML = productsHTML;
+
+let isStillTimeOut;
+let indexTimeOut;
+
+let stillTimeOutArray = Array(products.length).fill().map(v => false);
+let indexTimeOutArray = Array(products.length).fill();
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button, i) => {
+    button.addEventListener('click', () => {
+      const { productId } = button.dataset;
+      const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+      document.querySelector(`.js-added-to-cart-${productId}`)
+        .classList.add('show-element');
+
+      if (stillTimeOutArray[i]) {
+        clearTimeout(indexTimeOutArray[i]);
+      }
+      stillTimeOutArray[i] = true;
+
+      indexTimeOutArray[i] = setTimeout(() => {
+        if (stillTimeOutArray[i]) {
+          clearTimeout(indexTimeOutArray[i]);
+        }
+        document.querySelector(`.js-added-to-cart-${productId}`)
+          .classList.remove('show-element');
+        stillTimeOutArray[i] = false;
+      }, 2000);
+
+      let matchingItem;
+
+      cart.forEach(item => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += quantity;
+      } else {
+        cart.push({
+          productId,
+          quantity
+        });
+      }
+
+      let cartQuantity = 0;
+
+      cart.forEach(item => {
+        cartQuantity += item.quantity;
+      });
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+    })
+  });
